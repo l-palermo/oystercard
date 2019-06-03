4716b782669af18ba2a266f52b72b2265b922eb1
Luigi <l-palermo@hotmail.it>
2019-06-03 17:03:16 +0100

    Raise error if #top_up would exceed maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,18 @@
 class Oystercard
-
-  attr_reader :balance
+  attr_reader :balance, :BALANCE_LIMIT
+  BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
+  def exceed_limit?(number)
+    @balance + number > BALANCE_LIMIT
+  end
+
   def top_up(number)
+    raise "Top up declined. Limit balance would be exceed £#{BALANCE_LIMIT}" if exceed_limit?(number)
+
     @balance += number
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,7 +14,19 @@ RSpec.describe Oystercard do
 
   end
 
+  describe 'if #top up #exceed_limit?' do
+    it 'returns true if balance exceeded' do
+      expect(subject.exceed_limit?(91)).to eq true
+    end
+    
+  end
+
   describe '#top_up' do
+
+    it 'raise an error if top_up makes balance greater than LIMIT' do
+      limit = Oystercard::BALANCE_LIMIT
+      expect { subject.top_up(91) }.to raise_error "Top up declined. Limit balance would be exceed £#{limit}"
+    end
 
     it 'takes an argument and increment the balance' do
       expect(subject.top_up(3)).to eq(3)
