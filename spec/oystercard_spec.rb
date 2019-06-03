0c41f0b6236bd0f329ac6e4429a74312b5dbc82d
Luigi <l-palermo@hotmail.it>
2019-06-03 15:45:44 +0100

    Intialize oystercard with attribute #balance 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,13 @@ require 'oystercard'
 
 RSpec.describe Oystercard do
 
+  it 'responds to #balance' do
+    expect(subject.balance).to be_instance_of(Integer)
+  end
+
+  it 'has zero when intialized' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq(0)
+  end
+
 end
